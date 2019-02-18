4f4aabc18e1cf70f75116be74e02a9fe087061fe
sergioeabarcaf <sergioaf1991@gmail.com>
2019-02-18 02:06:08 -0300

    creacion de componentes base y fabricacion de current

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './components/map/map.component';
+import { CurrentComponent } from './components/current/current.component';
+import { History24HoursComponent } from './components/history24-hours/history24-hours.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    CurrentComponent,
+    History24HoursComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/history24-hours/history24-hours.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-history24-hours',
+  templateUrl: './history24-hours.component.html',
+  styleUrls: ['./history24-hours.component.css']
+})
+export class History24HoursComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
